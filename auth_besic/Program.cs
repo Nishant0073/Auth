e38871cbe9f4e1074f23e6dc6f6c62d2e41a7e71
Nishant Shingate <nishantks121@gmail.com>
2024-10-01 23:34:01 +0530

    custom auth with service & middleware

--- auth_besic/Program.cs
+++ auth_besic/Program.cs
@@ -1,29 +1,63 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-app.MapGet("/",()=> "Hello World!");
 
-app.MapGet("/username", (HttpContext httpContext,IDataProtectionProvider idp ) =>{
-
+app.Use((ctx,next)=>{
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = idp.CreateProtector("auth-cookie");
 
-    var authCookie = httpContext.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
     var payload = protector.Unprotect(authCookie.Split("=").Last());
-    var username = payload.Split(":").First();
-    var password = payload.Split(":").Last();
-    return password;
+    var  key = payload.Split(":").First();
+    var  value = payload.Split(":").Last();
+
+    var claims = new List<Claim>();
+    claims.Add(new Claim(key,value));
+    var identity = new ClaimsIdentity(claims);
+
+    ctx.User = new ClaimsPrincipal(identity);
+    return next();
 });
 
-app.MapGet("/login", (HttpContext httpContext,IDataProtectionProvider idp) =>{
-    var protector = idp.CreateProtector("auth-cookie");
-    httpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("nishant:1234")}" ;
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/username", (HttpContext httpContext) =>
+{
+
+    return httpContext.User.FindFirst("usr").Value;
+});
+
+app.MapGet("/login", (AuthService auth) =>
+{
+    auth.SignIn();
     return "Ok";
 });
 
 
 app.Run();
 
+
+public class AuthService
+{
+    public IHttpContextAccessor Accessor { get; }
+    private readonly IDataProtectionProvider idp;
+
+    public AuthService(IHttpContextAccessor accessor, IDataProtectionProvider idp)
+    {
+        Accessor = accessor;
+        this.idp = idp;
+    }
+    public void SignIn()
+    {
+        var protector = idp.CreateProtector("auth-cookie");
+        Accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:nishant")}";
+    }
+
+}
